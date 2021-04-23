d73f370c6cf9c86799bd091bff2a694ae0aa58e3
Danielioanitescu92 <daniel.ioanitescu@gmail.com>
2021-04-23 13:45:29 +0300

    2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edited 1
+          Edited 2
         </p>
         <a
           className="App-link"
